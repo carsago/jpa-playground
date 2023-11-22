5ddba75015019f95e0045a67607950377a252654
hyunseo <>
2023-11-22 16:27:01 +0900

    feat: DataSourceConfig 설정

--- src/main/java/com/example/jpastudy/domain/EventSender.java
+++
@@ -1,5 +0,0 @@
-package com.example.jpastudy.domain;
-
-public class EventSender {
-
-}
--- src/main/java/com/example/jpastudy/persistence/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jpastudy.persistence;
-
-import com.example.jpastudy.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
