a6cbfe52b87e1646536238523a48878da210befd
carsago <ohseo8813@gmail.com>
2023-12-05 17:39:30 +0900

    feat: 학습용 컨트롤러 생성

---
+++ src/main/java/com/example/jpastudy/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.jpastudy.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    private ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello, world");
+    }
+}
